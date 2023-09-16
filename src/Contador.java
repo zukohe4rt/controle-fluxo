d49fd44c1ec46366e0add7b53849f1f3486aaa25
Gustavo Herold <zukohe4rt@gmail.com>
2023-09-16 20:16:14 -0300

    update commit

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
         System.out.println("Digite o primeiro parâmetro");
